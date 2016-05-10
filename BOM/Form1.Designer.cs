a502ade1399d4f9fa012a27e06426b2fac20e82d
LukaszKubiak <kubiak.lukasz1993@gmail.com>
2016-05-10 21:12:42 +0200

    BOM

---
+++ BOM/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace BOM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 12);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open File";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ BOM/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BOM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                CalculateSheet();
+            }
+        }
+
+        private void CalculateSheet()
+        {
+            Excel.Application xlApp = new Excel.Application(); ;
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(openFileDialog1.FileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0); ;
+            Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            Excel.Range range = xlWorkSheet.UsedRange;
+            object misValue = System.Reflection.Missing.Value;
+
+            string str;
+            int rCnt = 0;
+            int cCnt = 0;
+            string[][] results = new string[range.Rows.Count - 16][];
+            for (int i = 0; i < range.Rows.Count - 16; i++)
+            {
+                results[i] = new string[range.Rows.Count - 16];
+            }
+
+            List<string> lista = new List<string>();
+            List<string> lista2 = new List<string>();
+
+            range.UnMerge();
+            for (rCnt = 18; rCnt < range.Rows.Count; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= 4; cCnt++)
+                {
+                   str = (range.Cells[rCnt, cCnt] as Excel.Range).Text;
+                    if (str == "" && (cCnt == 1 || cCnt == 2 || rCnt==18))
+                        continue;
+                    else
+                        lista.Add(str);          
+                }
+            }
+            rCnt = 18;
+            cCnt = 1;
+            str = (range.Cells[rCnt, cCnt] as Excel.Range).Text;
+           
+            while (rCnt < range.Rows.Count)
+            {
+                if (((range.Cells[rCnt, 1] as Excel.Range).Text == "" && (range.Cells[rCnt, 2] as Excel.Range).Text == ""))
+                    rCnt++;
+
+                lista2.Add((range.Cells[rCnt, 1] as Excel.Range).Text);
+                lista2.Add((range.Cells[rCnt, 2] as Excel.Range).Text);
+                if (!((range.Cells[rCnt, 3] as Excel.Range).Text == "" && (range.Cells[rCnt, 4] as Excel.Range).Text == ""))
+                {
+                    lista2.Add((range.Cells[rCnt, 3] as Excel.Range).Text);
+                    lista2.Add((range.Cells[rCnt, 4] as Excel.Range).Text);
+                }
+                while ((range.Cells[++rCnt, 2] as Excel.Range).Text == "" && rCnt < range.Rows.Count)
+                {
+                    if ((range.Cells[rCnt, 3] as Excel.Range).Text == "" && (range.Cells[rCnt, 4] as Excel.Range).Text == "")
+                        continue;
+                    else {
+                        lista2.Add((range.Cells[rCnt, 3] as Excel.Range).Text);
+                        lista2.Add((range.Cells[rCnt, 4] as Excel.Range).Text);
+                    }
+                }
+
+            }
+
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
+
